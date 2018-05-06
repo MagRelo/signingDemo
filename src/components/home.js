706e2536bec2adc03c6e44c456cb915cf6ba0bc6
Matt Lovan <magrelo404@gmail.com>
2018-05-06 18:54:49 -0400

    reorder home

--- src/components/home.js
+++ src/components/home.js
@@ -15,19 +15,19 @@ class HomeComponent extends Component {
           to reduce the amount of personal data is collected and stored.
         </p>        
 
-        <h3> <Link to="/chat"> User Chat</Link> </h3>        
+        <h3> <Link to="/chat"> Post signed messages</Link> </h3>        
         <p className="demo-description">
           Live chat with verifiable messages
         </p>
+
+        <h3><Link to="/admin">Admin panel</Link></h3>
+        <p className="demo-description">
+          Use signed messages to restrict access to a server API without requiring the user to login.
+        </p>        
         
         <h3><Link to="/preferences">User preferences</Link></h3>
         <p className="demo-description">
           Use the user's public key to remember their preferences without requiring the user to login.
-        </p>        
-
-        <h3><Link to="/admin">Admin panel</Link></h3>
-        <p className="demo-description">
-          Use digitally signed messages to restrict access to a server API without requiring the user to login.
         </p>        
         
         <h3><Link to="/sessions"> Sessions</Link></h3>        
@@ -35,6 +35,8 @@ class HomeComponent extends Component {
           Create a session token to let users "login" without needing an email or password.
         </p>
 
+        <hr/>
+        
         <h3> <Link to="/chat"> Dead-man switch</Link> </h3>        
         <p className="demo-description">
           Periodically post a message to declare that you are not under duress. If the message 
