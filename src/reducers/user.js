9a7e233d633e02a1c7fa8371a051a1a9f68455a0
Matt Lovan <magrelo404@gmail.com>
2018-05-09 12:35:59 -0400

    add GA

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,84 +1,78 @@
 import {
-  get as getCache, 
-  set as setCache, 
-  del as clearCache} from '../util/money-clip'
-import ethUtil  from 'ethereumjs-util'
-import moment from 'moment'
+  get as getCache,
+  set as setCache,
+  del as clearCache
+} from '../util/money-clip';
+import ethUtil from 'ethereumjs-util';
+import moment from 'moment';
 
-import store from '../store'
-
+import store from '../store';
 
 // Actions
 // ---
-export function loadPreferences(){
-  return function(dispatch){
+export function loadPreferences() {
+  return function(dispatch) {
+    const userAddress = store.getState().user.userAccount;
+    getCache('prefs-' + userAddress).then(val => {
+      // if no 'val' then we don't have a session or it has
+      //  expired => clear out the rest of the session data
+      if (!val) {
+        return dispatch({ type: 'LOCAL_PREFS_CLEAR' });
+      }
 
-    const userAddress = store.getState().user.userAccount
-    getCache('prefs-' + userAddress)
-      .then(val => {
-
-        // if no 'val' then we don't have a session or it has 
-        //  expired => clear out the rest of the session data
-        if(!val){                  
-          return dispatch({ type: 'LOCAL_PREFS_CLEAR' })
-        }
-
-        return dispatch({
-          type: 'LOCAL_PREFS_LOAD',
-          payload: JSON.parse(val)
-        })
-      })
-  }
+      return dispatch({
+        type: 'LOCAL_PREFS_LOAD',
+        payload: JSON.parse(val)
+      });
+    });
+  };
 }
-export function savePreferences(preferences){
-  return function(dispatch){
-
-    const userAddress = store.getState().user.userAccount
-    setCache('prefs-' + userAddress, JSON.stringify(preferences))
-      .then(val => {
-        return dispatch({
-          type: 'LOCAL_PREFS_LOAD',
-          payload: preferences
-        })
-      })
-  }
+export function savePreferences(preferences) {
+  return function(dispatch) {
+    const userAddress = store.getState().user.userAccount;
+    setCache('prefs-' + userAddress, JSON.stringify(preferences)).then(val => {
+      return dispatch({
+        type: 'LOCAL_PREFS_LOAD',
+        payload: preferences
+      });
+    });
+  };
 }
-export function clearPreferences(){
-  return function(dispatch){
-
-    const userAddress = store.getState().user.userAccount
-    clearCache('prefs-' + userAddress)
-      .then(val => {
-        return dispatch({ type: 'LOCAL_PREFS_CLEAR' })
-      })
-  }
+export function clearPreferences() {
+  return function(dispatch) {
+    const userAddress = store.getState().user.userAccount;
+    clearCache('prefs-' + userAddress).then(val => {
+      return dispatch({ type: 'LOCAL_PREFS_CLEAR' });
+    });
+  };
 }
 
-
-
-export function savePreferences_server(preferences){
-  return function(dispatch){
-    
-    const web3 = store.getState().web3.instance
-    const userAddress = store.getState().user.userAccount
+export function savePreferences_server(preferences) {
+  return function(dispatch) {
+    const web3 = store.getState().web3.instance;
+    const userAddress = store.getState().user.userAccount;
 
     // prepare the message for signing
-    const content = JSON.stringify(preferences)
-    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'))    
-    
+    const content = JSON.stringify(preferences);
+    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'));
+
     // sign message
-    web3.currentProvider.sendAsync({
+    web3.currentProvider.sendAsync(
+      {
         method: 'personal_sign',
         params: [contentHex, userAddress],
-        from: userAddress,
-      }, (err, result) => {
-        if (err) return console.error(err)
-        if (result.error) { return console.log('User denied signature.'); }
+        from: userAddress
+      },
+      (err, result) => {
+        if (err) return console.error(err);
+        if (result.error) {
+          return console.log('User denied signature.');
+        }
 
         const servesaHeader = {
-          'message': contentHex, 
-          'signature': result.result
-        }
+          message: contentHex,
+          signature: result.result
+        };
 
         // fetch
         return fetch('/api/user/preferences', {
@@ -89,39 +83,43 @@ export function savePreferences_server(preferences){
           },
           body: JSON.stringify(preferences)
         })
-        .then(response => response.json())
-        .then(responseBody => {
-          return dispatch({
-            type: 'SERVER_PREFS_LOAD',
-            payload: responseBody
-          })
-        })        
-
-      })
-  }
+          .then(response => response.json())
+          .then(responseBody => {
+            return dispatch({
+              type: 'SERVER_PREFS_LOAD',
+              payload: responseBody
+            });
+          });
+      }
+    );
+  };
 }
-export function clearPreferences_server(){
-  return function(dispatch){
-    const web3 = store.getState().web3.instance
-    const userAddress = store.getState().user.userAccount
+export function clearPreferences_server() {
+  return function(dispatch) {
+    const web3 = store.getState().web3.instance;
+    const userAddress = store.getState().user.userAccount;
 
     // prepare the message for signing
-    const content = JSON.stringify({action: 'delete user'})
-    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'))    
-    
+    const content = JSON.stringify({ action: 'delete user' });
+    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'));
+
     // sign message
-    web3.currentProvider.sendAsync({
+    web3.currentProvider.sendAsync(
+      {
         method: 'personal_sign',
         params: [contentHex, userAddress],
-        from: userAddress,
-      }, (err, result) => {
-        if (err) return console.error(err)
-        if (result.error) { return console.log('User denied signature.'); }
+        from: userAddress
+      },
+      (err, result) => {
+        if (err) return console.error(err);
+        if (result.error) {
+          return console.log('User denied signature.');
+        }
 
         const servesaHeader = {
-          'message': contentHex, 
-          'signature': result.result
-        }
+          message: contentHex,
+          signature: result.result
+        };
 
         // fetch
         return fetch('/api/user/delete', {
@@ -130,142 +128,145 @@ export function clearPreferences_server(){
             'x-servesa': JSON.stringify(servesaHeader)
           }
         })
-        .then(response => response.json())
-        .then(responseBody => {
-          return dispatch({
-            type: 'SERVER_PREFS_CLEAR'
-          })
-        })        
-
-      })
-  }
+          .then(response => response.json())
+          .then(responseBody => {
+            return dispatch({
+              type: 'SERVER_PREFS_CLEAR'
+            });
+          });
+      }
+    );
+  };
 }
 
+export function loadSession() {
+  return function(dispatch) {
+    const userAddress = store.getState().user.userAccount;
+    getCache('session-' + userAddress).then(val => {
+      // if no 'val' then we don't have a session or it has
+      //  expired => clear out the rest of the session data
+      if (!val) {
+        return dispatch({ type: 'SESSION_CLEAR' });
+      }
 
-
-export function loadSession(){
-  return function(dispatch){
-
-    const userAddress = store.getState().user.userAccount
-    getCache('session-' + userAddress)
-      .then(val => {
-
-        // if no 'val' then we don't have a session or it has 
-        //  expired => clear out the rest of the session data
-        if(!val){                  
-          return dispatch({ type: 'SESSION_CLEAR' })
-        }
-
-        return dispatch({
-          type: 'SESSION_LOAD',
-          payload: JSON.parse(val)
-        })
-      })
-  }
+      return dispatch({
+        type: 'SESSION_LOAD',
+        payload: JSON.parse(val)
+      });
+    });
+  };
 }
-export function clearSession(){
-  return function(dispatch){
-    
-    const userAddress = store.getState().user.userAccount
-    clearCache('session-' + userAddress)
-      .then(() => {        
-        return dispatch({ type: 'SESSION_CLEAR' })
-      })    
-  }
+export function clearSession() {
+  return function(dispatch) {
+    const userAddress = store.getState().user.userAccount;
+    clearCache('session-' + userAddress).then(() => {
+      return dispatch({ type: 'SESSION_CLEAR' });
+    });
+  };
 }
-export function saveSession(duration){
-  return function(dispatch){
-    
-    const web3 = store.getState().web3.instance
-    const userAddress = store.getState().user.userAccount
+export function saveSession(duration) {
+  return function(dispatch) {
+    const web3 = store.getState().web3.instance;
+    const userAddress = store.getState().user.userAccount;
 
     // calculate expiration
     const expiration = moment().add(duration, 'minutes');
 
     // prepare the message for signing
-    const content = `{"expires": "${expiration.toISOString()}"}`
-    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'))    
-    
+    const content = `{"expires": "${expiration.toISOString()}"}`;
+    const contentHex = ethUtil.bufferToHex(new Buffer(content, 'utf8'));
+
     // sign message
-    web3.currentProvider.sendAsync({
+    web3.currentProvider.sendAsync(
+      {
         method: 'personal_sign',
         params: [contentHex, userAddress],
-        from: userAddress,
-      }, (err, result) => {
-        if (err) return console.error(err)
-        if (result.error) { return console.log('User denied signature.'); }
-
-        const sessionData = {
-          'message': contentHex, 
-          'signature': result.result, 
-          'duration': duration,
-          'expires': expiration.toISOString()     
+        from: userAddress
+      },
+      (err, result) => {
+        if (err) return console.error(err);
+        if (result.error) {
+          return console.log('User denied signature.');
         }
 
+        const sessionData = {
+          message: contentHex,
+          signature: result.result,
+          duration: duration,
+          expires: expiration.toISOString()
+        };
+
         // save session to IndexedDB
-        const staleAfter = Date.now() + (duration * 60 * 1000)
-        setCache('session-' + userAddress, JSON.stringify(sessionData), {'staleAfter': staleAfter})
-          .then(val => {
-            return dispatch({
-              type: 'SESSION_LOAD',
-              payload: sessionData
-            })
-          })
-
-      })
-  }
+        const staleAfter = Date.now() + duration * 60 * 1000;
+        setCache('session-' + userAddress, JSON.stringify(sessionData), {
+          staleAfter: staleAfter
+        }).then(val => {
+          return dispatch({
+            type: 'SESSION_LOAD',
+            payload: sessionData
+          });
+        });
+      }
+    );
+  };
 }
-
 
 // Reducers
 // ---
 const initialState = {
   userAccount: '',
   text: 'medium',
-  theme: 'light', 
-  clientSaved: false, 
-  message: '', 
-  signature: '', 
+  theme: 'light',
+  clientSaved: false,
+  message: '',
+  signature: '',
   duration: 0,
-  expires: null  
-}
+  expires: null
+};
 
-const userReducer = (state = initialState, action) => {  
-  if (action.type === 'WEB3_INITIALIZED'){    
-    return Object.assign({}, state, {'userAccount': action.payload.accounts[0]})
+const userReducer = (state = initialState, action) => {
+  if (action.type === 'WEB3_INITIALIZED') {
+    let gtag = window.gtag;
+    if (gtag) {
+      console.log('party!');
+      gtag('set', { user_id: action.payload.accounts[0] });
+    }
+    return Object.assign({}, state, {
+      userAccount: action.payload.accounts[0]
+    });
   }
-  
-  if (action.type === 'LOCAL_PREFS_LOAD'){    
-    return Object.assign({}, state, action.payload, {clientSaved: true})
+
+  if (action.type === 'LOCAL_PREFS_LOAD') {
+    return Object.assign({}, state, action.payload, { clientSaved: true });
   }
-  if (action.type === 'LOCAL_PREFS_CLEAR'){    
+  if (action.type === 'LOCAL_PREFS_CLEAR') {
     return Object.assign({}, state, action.payload, {
       clientSaved: false,
       theme: 'light',
       text: 'medium'
-    })
+    });
   }
 
-  if (action.type === 'SERVER_PREFS_LOAD'){    
-    return Object.assign({}, state, action.payload, {serverSaved: true})
+  if (action.type === 'SERVER_PREFS_LOAD') {
+    return Object.assign({}, state, action.payload, { serverSaved: true });
   }
-  if (action.type === 'SERVER_PREFS_CLEAR'){    
-    return Object.assign({}, state, {serverSaved: false})
+  if (action.type === 'SERVER_PREFS_CLEAR') {
+    return Object.assign({}, state, { serverSaved: false });
   }
 
-  if (action.type === 'SESSION_LOAD'){    
-    return Object.assign({}, state, action.payload)
+  if (action.type === 'SESSION_LOAD') {
+    return Object.assign({}, state, action.payload);
   }
-  if (action.type === 'SESSION_CLEAR'){    
+  if (action.type === 'SESSION_CLEAR') {
     return Object.assign({}, state, {
-      message: '', 
-      signature: '', 
+      message: '',
+      signature: '',
       duration: 0,
       expires: null
-    })    
+    });
   }
 
-  return state
-}
+  return state;
+};
 
-export default userReducer
+export default userReducer;
