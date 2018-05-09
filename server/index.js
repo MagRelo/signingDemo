c97b2943fc486d5ab288e4a2be373e217b018da9
Matt Lovan <magrelo404@gmail.com>
2018-05-08 20:08:08 -0400

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = require('express')();
 const server = require('http').Server(app);
@@ -7,8 +6,8 @@ const morgan = require('morgan');
 
 const io = require('socket.io')(server);
 
-const sigUtil = require('eth-sig-util')
-const ethUtil = require('ethereumjs-util')
+const sigUtil = require('eth-sig-util');
+const ethUtil = require('ethereumjs-util');
 
 // *
 // db
@@ -17,193 +16,213 @@ const ethUtil = require('ethereumjs-util')
 // Connect to mongoDb
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL_INT || 'mongodb://127.0.0.1:27017/signdemo');
-mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-	}
+mongoose.connect(
+  process.env.MONGODB_URL_INT || 'mongodb://127.0.0.1:27017/signdemo'
 );
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+});
 
 // setup Mongoose message schema & model
-const MessageSchema = new mongoose.Schema({
-  messageParams: Object,
-  signature: String,
-  userAddress: String,
-  content: String
-}, {timestamps: true});
+const MessageSchema = new mongoose.Schema(
+  {
+    messageParams: Object,
+    signature: String,
+    userAddress: String,
+    content: String
+  },
+  { timestamps: true }
+);
 const MessageModel = mongoose.model('Message', MessageSchema);
 
-const UserSchema = new mongoose.Schema({  
-  userAddress: String,
-  text: String,
-  theme: String
-}, {timestamps: true});
+const UserSchema = new mongoose.Schema(
+  {
+    userAddress: String,
+    text: String,
+    theme: String
+  },
+  { timestamps: true }
+);
 const UserModel = mongoose.model('User', UserSchema);
-
 
 // *
 // Server & http routing
 // *
 
 // configure express middleware
-app.use(express.static('build'))
+app.use(express.static('build'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({limit: '1mb'}));
-app.use(morgan('dev', {
-  skip: function (req, res) {
-    // remove the frontend dev server's 'json' calls from the console output
-    return req.originalUrl.indexOf('json') > 0
-  }
-}));
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(
+  morgan('dev', {
+    skip: function(req, res) {
+      // remove the frontend dev server's 'json' calls from the console output
+      return req.originalUrl.indexOf('json') > 0;
+    }
+  })
+);
 
 // start server
 server.listen(8080, () => {
-  console.log('server listening on 8080');  
+  console.log('server listening on 8080');
 });
 
-// User
-
 // get user preferences
-app.get('/api/user/preferences', servesaAuth, function(req, res){
-  
-  const expirationDate = new Date(req.userMessage.expires)
-  if(expirationDate < Date.now()){    
-    return res.status(401).send([])
+app.get('/api/user/preferences', servesaAuth, function(req, res) {
+  const expirationDate = new Date(req.userMessage.expires);
+  if (expirationDate < Date.now()) {
+    return res.status(401).send([]);
   }
 
-  MessageModel.find({'userAddress': req.userAddress})
-    .then(messages => {return res.send(messages)})
-    .catch(error => {return res.status(500).send(error)})      
-})
+  MessageModel.find({ userAddress: req.userAddress })
+    .then(messages => {
+      return res.send(messages);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+});
 
 // save user preferences
-app.post('/api/user/preferences', servesaAuth, function(req, res){
-
+app.post('/api/user/preferences', servesaAuth, function(req, res) {
   const userUpdate = {
     userAddress: req.userAddress,
     text: req.body.text,
     theme: req.body.theme
-  }
-  
-  // update user 
-  UserModel.update({userAddress: req.userAddress}, userUpdate, {upsert: true})
-    .then(result => {return res.send(result)})
-    .catch(error => {return res.status(500).send(error)}) 
-})
+  };
+
+  // update user
+  UserModel.update({ userAddress: req.userAddress }, userUpdate, {
+    upsert: true
+  })
+    .then(result => {
+      return res.send(result);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+});
 
 // delete user preferences
-app.delete('/api/user/delete', servesaAuth, function(req, res){
-
-  // update user 
-  UserModel.remove({userAddress: req.userAddress})
-    .then(result => {return res.send(result)})
-    .catch(error => {return res.status(500).send(error)}) 
-})
-
-
-// Messages
+app.delete('/api/user/delete', servesaAuth, function(req, res) {
+  // update user
+  UserModel.remove({ userAddress: req.userAddress })
+    .then(result => {
+      return res.send(result);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+});
 
 // message detail
-app.get('/api/messages/:id', function(req, res){
-
-  MessageModel.findOne({_id: req.params.id})
-    .then(message => {return res.send(message)})
-    .catch(error => {return res.status(500).send(error)})
-})
-
-app.get('/*', function(req, res){
-  res.sendFile('index.html', { root: './build'});
+app.get('/api/messages/:id', function(req, res) {
+  MessageModel.findOne({ _id: req.params.id })
+    .then(message => {
+      return res.send(message);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
 });
 
 // delete all messages
-app.post('/api/messages/delete', servesaAuth, function(req, res){
-
+app.post('/api/messages/delete', servesaAuth, function(req, res) {
   // auth
-  if(req.userAddress != '0x863afa452f38966b54cb1149d934e34670d0683a'){
-    return res.status(401).send({})
+  if (req.userAddress != '0x863afa452f38966b54cb1149d934e34670d0683a') {
+    return res.status(401).send({});
   }
-  
+
   // delete all messages
   MessageModel.remove()
-    .then(result => {return res.send(result)})
-    .catch(error => {return res.status(500).send(error)}) 
-})
+    .then(result => {
+      return res.send(result);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
+    });
+});
 
-
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', { root: './build' });
+});
 
 // *
 // sockets routing
 // *
 
-io.on('connection', function (socket) {
-
+io.on('connection', function(socket) {
   // send out fresh data on connect
-  MessageModel.find().sort({'updatedAt': 1})
-    .then(messageArray => {return socket.emit('update', messageArray)})
-    .catch(error => {return socket.emit('error', error.message)})
+  MessageModel.find()
+    .sort({ updatedAt: 1 })
+    .then(messageArray => {
+      return socket.emit('update', messageArray);
+    })
+    .catch(error => {
+      return socket.emit('error', error.message);
+    });
 
-
-  socket.on('message', function (data) {
-
+  socket.on('message', function(data) {
     // authenticate (verify)
     const userAddress = sigUtil.recoverPersonalSignature({
       data: data.message,
       sig: data.signature
-    })
-    
+    });
+
     // save message
     const message = new MessageModel({
-      'userAddress': userAddress,
-      'messageParams': data.message,
-      'signature': data.signature,      
-      'content': ethUtil.toBuffer(data.message).toString('utf8')
-    })
+      userAddress: userAddress,
+      messageParams: data.message,
+      signature: data.signature,
+      content: ethUtil.toBuffer(data.message).toString('utf8')
+    });
 
-    message.save()
-      .then(savedMessage => {        
-        return MessageModel.find().sort({'updatedAt': 1})
+    message
+      .save()
+      .then(savedMessage => {
+        return MessageModel.find().sort({ updatedAt: 1 });
       })
       .then(messageArray => {
-        return socket.emit('update', messageArray)
+        return socket.emit('update', messageArray);
       })
-      .catch(error => {return socket.emit('error', error.message)})
-
-  })
-
+      .catch(error => {
+        return socket.emit('error', error.message);
+      });
+  });
 });
-
-
 
 // *
 // helpers
 // *
 
-function servesaAuth(req, res, next){
-
+function servesaAuth(req, res, next) {
   // check for header
-  if(!req.headers['x-servesa']){    
-    return res.status(401).send([])
+  if (!req.headers['x-servesa']) {
+    return res.status(401).send([]);
   }
 
   // parse header object
-  const authObject = JSON.parse(req.headers['x-servesa'])
-  if(!authObject.message || !authObject.signature){    
-    return res.status(401).send([])
+  const authObject = JSON.parse(req.headers['x-servesa']);
+  if (!authObject.message || !authObject.signature) {
+    return res.status(401).send([]);
   }
 
   // recover public key
   const userAddress = sigUtil.recoverPersonalSignature({
     data: authObject.message,
     sig: authObject.signature
-  })
+  });
 
   // parse message & check timestamp for expiration
-  const message = JSON.parse(ethUtil.toBuffer(authObject.message).toString('utf8'))
+  const message = JSON.parse(
+    ethUtil.toBuffer(authObject.message).toString('utf8')
+  );
 
   // pass along userAddress and message
-  req.userAddress = userAddress
-  req.userMessage = message
+  req.userAddress = userAddress;
+  req.userMessage = message;
 
   // call next middleware function
-  next()
+  next();
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,59 +1,50 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class HomeComponent extends Component {
   render() {
-    return(
-      
+    return (
       <div className="home">
-
         <h1>Digital Signature Demos</h1>
-        
-        <hr/>
+
+        <hr />
         <p>
           Digital signatures can be used in place of email and password logins
-          to reduce the amount of personal data is collected and stored. 
-        </p>        
-
-        <h3> <Link to="/chat"> Post signed messages</Link> </h3>        
-        <p className="demo-description">
-          Live chat with verifiable messages
+          to reduce the amount of personal data is collected and stored.
         </p>
 
-        <h3><Link to="/admin">Admin panel</Link></h3>
+        <h3>
+          {' '}
+          <Link to="/chat"> Post signed messages</Link>{' '}
+        </h3>
+        <p className="demo-description">Live chat with verifiable messages</p>
+
+        <h3>
+          <Link to="/admin">Admin panel</Link>
+        </h3>
         <p className="demo-description">
-          Use signed messages to restrict access to a server API without requiring the user to login.
-        </p>        
-        
-        <h3><Link to="/preferences">User preferences</Link></h3>
-        <p className="demo-description">
-          Use the user's public key to remember their preferences without requiring the user to login.
-        </p>        
-        
-        <h3><Link to="/sessions"> Sessions</Link></h3>        
-        <p className="demo-description">
-          Create a session token to let users "login" without needing an email or password.
+          Use signed messages to restrict access to a server API without
+          requiring the user to login.
         </p>
 
-        <h3>Transfer control of account (coming soon)</h3>        
+        <h3>
+          <Link to="/preferences">User preferences</Link>
+        </h3>
         <p className="demo-description">
-          Users can create a new identity and transfer some or all of their data to the new identity.
+          Use the user's public key to remember their preferences without
+          requiring the user to login.
         </p>
 
-        <hr/>
-        
-        <h3> Dead-man switch (coming soon) </h3>        
+        <h3>
+          <Link to="/sessions"> Sessions</Link>
+        </h3>
         <p className="demo-description">
-          Periodically post a message to declare that you are not under duress. If the message 
-          fails to appear then observers will know that something is wrong (
-          <a href="https://www.theguardian.com/technology/2013/sep/09/nsa-sabotage-dead-mans-switch" target="_blank" rel="noopener noreferrer">as suggested </a>
-          by Cory Doctrow.)
+          Create a session token to let users "login" without needing an email
+          or password.
         </p>
-
       </div>
-    )
+    );
   }
 }
 
-
-export default HomeComponent
+export default HomeComponent;
