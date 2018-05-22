fd8e30bf931c9ab0e41be874283ce3157211b2f4
Matt Lovan <magrelo404@gmail.com>
2018-05-22 16:16:47 -0400

    update home

--- src/components/home.js
+++ src/components/home.js
@@ -8,31 +8,30 @@ class HomeComponent extends Component {
         <h1>Servésa Labor Market</h1>
         <hr />
 
-        <p>
-          Servésa a new way to find and retain staff for your organization. Your
-          company can buy tokens that can be redeemed for a unit for work by the
-          person that issues the token.
-        </p>
-
         <h2>Thesis</h2>
         <p>
-          By radically increasing transperancy around the availablilty, pricing
-          and scheduling of labor we can unlock new opportunities for profitable
-          cooperation. As this strategy focuses on reducing transaction costs we
-          expect it will be most valuable when applied to areas where a) work is
-          intermittent, and b) there is high transaction costs around finding
-          and retaining labor (such as high-value knowledge work). This strategy
-          also creates pricing signals to further improve effeciency; these
-          signals will be most effective where the services are somewhat
-          interchangeable but difficult to directly compare (e.g., developers).
+          By increasing transperancy around the availablilty, pricing and
+          scheduling of labor we can unlock new opportunities for profitable
+          cooperation. We'll create a unique token to represent each agent's
+          unit of work. We'll then create financial incentives around how tokens
+          are priced and redeemed to encourage cooperation between prinicipals
+          and agents.
+        </p>
+        <p>
+          As this strategy focuses on reducing transaction costs we expect it
+          will be most valuable when applied to areas where a) there is high
+          transaction costs around finding and retaining labor, b) work is
+          intermittent, and c) where the services are somewhat interchangeable
+          but difficult to compare directly (e.g., software developers).
         </p>
 
         <h2>Example</h2>
         <ol>
           <li>
             <p>
-              Alice is smart contract security specialist. She is available for
-              contract work so she creates a profile on Servesa.
+              Alice is smart contract security specialist. She creates a profile
+              on Servesa to advertise her availablity and to handle the billing
+              for her services.
             </p>
             <p className="inset">
               <em>
@@ -41,11 +40,11 @@ class HomeComponent extends Component {
                 <a href="https://hackernoon.com/an-overview-of-non-fungible-tokens-5f140c32a70a">
                   non-fungible token
                 </a>{' '}
-                to represent Alice's work. Each token is unique and sequential
-                and the sequence number represents the order that Alice is
-                expected to redeem them. If, for example, two token holders were
-                both trying to schedule Alice, Alice is expected to schedule the
-                work for the earlier tokens first. There is no requirement to
+                to represent Alice's work. Each token is sequential and the
+                sequence number represents the order that Alice is expected to
+                redeem them. If, for example, two token holders were both trying
+                to schedule Alice for an upcoming project, Alice is expected to
+                honor the earlier tokens first. There is no requirement to
                 redeem the tokens in order – it simply acts as a signal to help
                 Alice and the token holders coordinate scheduling (it is also in
                 Alice's economic interest for her to honor the priority of the
@@ -57,8 +56,8 @@ class HomeComponent extends Component {
             <p>
               Bob manages a team that is developing smart contracts and he knows
               that he'll eventually need a security audit. He'd like to find and
-              retain a security expert in the meantime and then have them begin
-              as soon as possible after each contract is ready but but he's not
+              retain a security expert ahead of time and also have them begin as
+              soon as possible after each contract is ready but but he's not
               sure when all of the contracts will be finished.
             </p>
 
@@ -122,20 +121,62 @@ class HomeComponent extends Component {
 
         <h2>Advantages for pricipals (Bob)</h2>
         <ul>
-          <li>Only pay for the time that you use</li>
-          <li>Maintain long-term relationships with fluid team memebers</li>
-          <li>Build tacit knowledge in the organization</li>
-          <li>Find and retain employees before they are needed</li>
+          <li>
+            <p>
+              Only pay for the time that you use, and only pay when you use it
+            </p>
+          </li>
+          <li>
+            <p>Find and retain employees before they are needed</p>
+          </li>
+          <li>
+            Possibility of making a profit if the early tokens become more
+            valuable
+          </li>
         </ul>
 
         <h2>Advantages for agents (Alice) </h2>
         <ul>
           <li>
-            Allow market to set prices for her services based on demand - no
-            negotiation
+            <p>
+              Allow market to set prices for an agent's services based on
+              demand. The 'sell' price of the token will include both the base
+              rate for their services as well as the premium that token-holders
+              pay to hold early tokens (i.e., to hold their place in line).
+            </p>
           </li>
-          <li />
+          <li>
+            <p>More flexibility in when and how to work</p>
+          </li>
+          <li>
+            <p>Verifiable signals of quality and demand for services</p>
+          </li>
         </ul>
+
+        <h2>Market Dynamics</h2>
+
+        <h3>Incentive to hold tokens</h3>
+        <p>
+          For principals, buying and holding tokens means that they pay less for
+          each token (curved-bonding pricing) and that they have higher priority
+          when they choose to redeem the service.
+        </p>
+
+        <h3>Speculation</h3>
+        <p>
+          This could create an incentive for secondary markets to emerge by
+          identifying and flipping under-priced services. This may be a poitive
+          feature of the system
+        </p>
+
+        <h3>Market-tuning parameters</h3>
+        <p>
+          It mat be desirable to introduce fees to shape market dynamics. If
+          speculation begins to cause too much churn we could introduce 'buy'
+          and 'sell' taxes to reduce velocity. If principals are holding too
+          many tokens we could introduce demmurage to increase the cost of
+          carrying capital.
+        </p>
 
         <h2>Demo</h2>
         <p>
